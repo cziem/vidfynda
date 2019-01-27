ac4b187dd36cbf2d55cc689c55c17bb3277a6d49
Fayvor George <phavorsparks@gmail.com>
2019-01-27 01:53:14 +0100

    update file structure

--- controllers/app.js
+++ controllers/app.js
@@ -1,3 +1,4 @@
+require('../config/config')
 const request = require('request')
 
 const apikey = process.env.API_KEY
@@ -11,14 +12,14 @@ module.exports = {
     const uri = `http://www.omdbapi.com/?apikey=${apikey}&s=${movieName}`
   
     request(uri, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const data = JSON.parse(body)
-          if (data.Response.toLowerCase() === 'false') {
-            res.render('error', { data: data.Error })
-          } else {
-            res.render('results', { data })
-          }
+      if (!error && response.statusCode === 200) {
+        const data = JSON.parse(body)
+        if (data.Response.toLowerCase() === 'false') {
+          res.render('error', { data: data.Error })
+        } else {
+          res.render('results', { data })
         }
-      })
+      }
+    })
   }
 }
