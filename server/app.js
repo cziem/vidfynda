576797509e44e995f693f37fee6a736c036e99d2
Fayvor George <phavorsparks@gmail.com>
2019-01-28 16:57:05 +0100

    clean up redundant routes

--- server/app.js
+++ server/app.js
@@ -16,28 +16,6 @@ app.get('/', (req, res) => {
   res.render("home")
 })
 
-app.get('/friends', (req, res) => {
-  res.render('friends', { friends: friendsList })
-})
-
-app.post('/addFriend', (req, res) => {
-  const friend = req.body.friend
-  friendsList.push(friend)
-  res.redirect('/friends')
-})
-
-app.get('/posts', (req, res) => {
-  let posts = [
-    { title: "Things we do for love", author: "Lovern Ralf" },
-    { title: "Asthetics and Approach", author: "Rulda Wliaz" },
-    { title: "Difinitive Guide to Design", author: "Marqueze Jerry" },
-    { title: "All For One", author: "Stella Grand" },
-    { title: "The Grannys", author: "Bezos" }
-  ]
-
-  res.render("post", { posts })
-})
-
 // Movie API
 app.use('/movies', movieRouter)
 app.use('/movieResults', movieRouter)
